d8dcc4171153c6a883c733ebc4c9fb6ff991880b
hyxdev <kaiyoung6-c@my.cityu.edu.hk>
2023-05-12 17:58:19 +0800

    third

---
+++ 41 Generste Random Whole Numbers Within a Range.js
@@ -0,0 +1,10 @@
+
+
+function randomRange (myMin, myMax){
+
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+var myRandom = randomRange(5, 15);
+
+console.log(myRandom);
---
+++ 43 use the parseInt Function with a Radi.js
@@ -0,0 +1,6 @@
+function convertToInteger(Str){
+return parseInt(Str, 2)
+
+}
+convertToInteger("10011");
+console.log(convertToInteger("10011"));
